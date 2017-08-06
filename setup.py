00d9b221e7612b8e8b9161fcfe0520873cc9f70a
Brett Sandler <sandler.brett@gmail.com>
2017-08-06 17:07:56 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='lemonpy',
+    version='0.1.0',
+    packages=['lemonpy'],
+    license='MIT License',
+    long_description=open('README.mkd').read(),
+)
